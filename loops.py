c00737a9767236afae85efc14b3b6b8fc41105db
root <root@localhost.localdomain>
2021-08-16 10:00:41 +0000

    Test

---
+++ loops.py
@@ -0,0 +1,26 @@
+count = 0
+
+i = 10
+
+test = ['monkey','dog','practice']
+
+
+for x in range(i):
+    count = count+1
+    print(x)
+    if count == 5:
+        print(f'we are halfway, count = {count}')
+    
+
+
+for u in test:
+    print(u)
+
+n = []
+
+
+
+for j in range(i):
+    n.append(j)
+
+print(n)
